1290660ea5ff7f5a4b96e4e098eb80d08a10f621
opion <kindledkindred@gmail.com>
2018-12-27 01:00:43 +0900

    start w/ vue and firebase
    vue
    vue-router
    vuex
    firebase

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyAMfuWAXlAhQC4eHqPE17wy6J83p2cG5I4",
+  authDomain: "wraithia-trpg.firebaseapp.com",
+  databaseURL: "https://wraithia-trpg.firebaseio.com",
+  projectId: "wraithia-trpg",
+  storageBucket: "wraithia-trpg.appspot.com",
+  messagingSenderId: "1078050853384"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
